94decdc4018dae20c2f7aad97d795ddbf5f7eabd
Nadia Salem <nsalemprofessional@gmail.com>
2026-05-24 15:33:05 -0700

    Add detected labels for spike detection

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ for i in range(200):
         spike = -1 * spike
     value = wave + noise + spike
     data.append(value)
+detected_labels = [0] * len(data)
 for i in range(1, len(data)-1):
     prev = data[i-1]
     cur = data[i]
     nextVal = data[i+1]
     if(abs(cur - prev) > threshold and abs(cur - nextVal) > threshold):
         spike_count += 1
+        detected_labels[i] = 1
     if((cur > 1 or cur < -1) and (prev > 1 or prev < -1) and (nextVal > 1 or nextVal < -1) ):
         burst_count += 1
-for value in data:
-    print(i)
 plt.plot(data)
 plt.title("Plot Random")
 plt.show()
